90155e6ba492fee71dc6e0217291c8ab488e97bb
Petr <petrobatakshov@yandex.ru>
2025-03-30 07:21:55 +0300

    fixed Category method 'add_product'

--- main.py
+++ main.py
@@ -54,7 +54,7 @@ class Category:
     def add_product(self, value: list | Product):
 
         self.__products.append(value)
-        self.product_count = len(self.__products)
+        self.product_count += 1
 
     @property
     def products(self):
@@ -80,23 +80,18 @@ if __name__ == "__main__":
     category1 = Category(
         "Смартфоны",
         "Смартфоны, как средство не только коммуникации, но и получения дополнительных функций для удобства жизни",
-        [product1, product2, product3],
+        [product1, product2, product3]
     )
 
     print(category1.products)
-    product4 = Product('55" QLED 4K', "Фоновая подсветка", 123000.0, 7)
+    product4 = Product("55\" QLED 4K", "Фоновая подсветка", 123000.0, 7)
     category1.add_product(product4)
     print(category1.products)
     print(category1.product_count)
 
     new_product = Product.new_product(
-        {
-            "name": "Samsung Galaxy S23 Ultra",
-            "description": "256GB, Серый цвет, 200MP камера",
-            "price": 180000.0,
-            "quantity": 5,
-        }
-    )
+        {"name": "Samsung Galaxy S23 Ultra", "description": "256GB, Серый цвет, 200MP камера", "price": 180000.0,
+         "quantity": 5})
     print(new_product.name)
     print(new_product.description)
     print(new_product.price)
