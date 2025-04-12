d31dba6084c253a4076fb9c574093788effa1cda
Petr <petrobatakshov@yandex.ru>
2025-04-12 21:54:30 +0300

    adding new classes to the dependency. fixed tests

--- main.py
+++ main.py
@@ -1,21 +1,25 @@
-class Product:
+from src.basic_classes import BaseProduct
+from src.mixin_classes import PrintMixin
+
+
+class Product(BaseProduct, PrintMixin):
     name: str
     description: str
     price: float
     quantity: int
 
     def __init__(self, name: str, description: str, price: float, quantity: int):
-        """ """
         self.name = name
         self.description = description
         self.__price = price
         self.quantity = quantity
+        super().__init__()
 
     def __str__(self):
         return f"{self.name}, {self.__price} руб. Остаток: {self.quantity} шт."
 
     def __add__(self, other):
-        if type(self) == type(other):
+        if type(other) is type(self):
             cost_of_products_1 = self.quantity * self.__price
             cost_of_products_2 = other.quantity * other.__price
             return cost_of_products_1 + cost_of_products_2
@@ -54,14 +58,14 @@ class Category:
     product_count = 0
 
     def __init__(self, name: str, description: str, products: list):
-        """ """
         self.name = name
         self.description = description
         self.__products = products
 
         self.category_count += [name]
         for v in products:
-            self.product_count += 1
+            if v:
+                self.product_count += 1
 
     def __str__(self):
         result = []
@@ -69,15 +73,13 @@ class Category:
             result.append(f"{i.name}, {i.price} руб. Остаток: {i.quantity} шт.")
         return "\n".join(result)
 
-
     def add_product(self, value):
         if not isinstance(value, Product):
-            raise ValueError
+            raise TypeError
         else:
             self.product_count += 1
             return self.__products.append(value)
-  
-  
+
     @property
     def products(self):
         return self.__products
@@ -144,13 +146,12 @@ class LawnGrass(Product):
 
 
 if __name__ == "__main__":
-  
+
     # ___ проверка задания 14,1
 
     product1 = Product("Samsung Galaxy S23 Ultra", "256GB, Серый цвет, 200MP камера", 180000.0, 5)
     product2 = Product("Iphone 15", "512GB, Gray space", 210000.0, 8)
     product3 = Product("Xiaomi Redmi Note 11", "1024GB, Синий", 31000.0, 14)
-
 
     print(product1.name)
     print(product1.description)
@@ -170,7 +171,6 @@ if __name__ == "__main__":
     print(str(product1))
     print(str(product2))
     print(str(product3))
-
 
     category1 = Category(
         "Смартфоны",
@@ -232,8 +232,8 @@ if __name__ == "__main__":
     print(new_product.price)
     print(new_product.quantity)
 
-    new_product.price = 800
-    print(new_product.price)
+    # new_product.price = 800
+    # print(new_product.price)
 
     new_product.price = -100
     print(new_product.price)
@@ -345,8 +345,54 @@ if __name__ == "__main__":
     else:
         print("Не возникла ошибка TypeError при добавлении не продукта")
 
-
     print(product1 + product2)
     print(product1 + product3)
     print(product2 + product3)
 
+    # ------ проверка задания 16.2
+    product1 = Product("Samsung Galaxy S23 Ultra", "256GB, Серый цвет, 200MP камера", 180000.0, 5)
+    product2 = Product("Iphone 15", "512GB, Gray space", 210000.0, 8)
+    product3 = Product("Xiaomi Redmi Note 11", "1024GB, Синий", 31000.0, 14)
+
+    print(product1.name)
+    print(product1.description)
+    print(product1.price)
+    print(product1.quantity)
+
+    print(product2.name)
+    print(product2.description)
+    print(product2.price)
+    print(product2.quantity)
+
+    print(product3.name)
+    print(product3.description)
+    print(product3.price)
+    print(product3.quantity)
+
+    category1 = Category(
+        "Смартфоны",
+        "Смартфоны, как средство не только коммуникации, но и получения дополнительных функций для удобства жизни",
+        [product1, product2, product3],
+    )
+
+    print(category1.name == "Смартфоны")
+    print(category1.description)
+    print(len(category1.products))
+    print(category1.category_count)
+    print(category1.product_count)
+
+    product4 = Product('55" QLED 4K', "Фоновая подсветка", 123000.0, 7)
+
+    category2 = Category(
+        "Телевизоры",
+        "Современный телевизор, который позволяет наслаждаться просмотром, станет вашим другом и помощником",
+        [product4],
+    )
+
+    print(category2.name)
+    print(category2.description)
+    print(len(category2.products))
+    print(category2.products)
+
+    print(Category.category_count)
+    print(Category.product_count)
--- tests/test_main.py
+++ tests/test_main.py
@@ -1,15 +1,17 @@
 import pytest
 
-from main import Category, Product, Smartphone, LawnGrass
+from main import Category, LawnGrass, Product, Smartphone
 
 
 @pytest.fixture
 def product1() -> Product:
+    """Fixture телефон samsung"""
     return Product("Samsung Galaxy S23 Ultra", "256GB, Серый цвет, 200MP камера", 180000.0, 5)
 
 
 @pytest.fixture
 def category_tv(product1: Product) -> Category:
+    """Fixture категория телевизоры"""
     return Category(
         "Телевизоры",
         "Современный телевизор, который позволяет наслаждаться просмотром, станет вашим другом и помощником",
@@ -18,39 +20,46 @@ def category_tv(product1: Product) -> Category:
 
 
 @pytest.fixture
-def product_smartphone1():
+def product_smartphone1() -> Smartphone:
+    """Fixture смартфон1 Iphone"""
     return Smartphone("Iphone 15", "512GB, Gray space", 210000.0, 8, 98.2, "15", 512, "Gray space")
 
 
 @pytest.fixture
-def product_smartphone2():
+def product_smartphone2() -> Smartphone:
+    """Fixture смартфон2 Samsung"""
     return Smartphone(
         "Samsung Galaxy S23 Ultra", "256GB, Серый цвет, 200MP камера", 180000.0, 5, 95.5, "S23 Ultra", 256, "Серый"
     )
 
 
 @pytest.fixture
-def product_grass1():
+def product_grass1() -> LawnGrass:
+    """Fixture газонная трава"""
     return LawnGrass("Газонная трава", "Элитная трава для газона", 500.0, 20, "Россия", "7 дней", "Зеленый")
 
 
 @pytest.fixture
 def product_tv():
+    """Fixture телевизор"""
     return Product('55" QLED 4K', "Фоновая подсветка", 123000.0, 7)
 
 
 @pytest.fixture
-def category_smartphones2(product_smartphone1):
+def category_smartphones1(product_smartphone1):
+    """Fixture категория смарфоны"""
     return Category("Смартфоны", "Высокотехнологичные смартфоны", [product_smartphone1])
 
 
 @pytest.fixture
 def user_value():
+    """Fixture значение пользователя: 800"""
     return 800
 
 
 @pytest.fixture
 def new_product() -> Product:
+    """Fixture данные продукта в словаре. Смартфон samsung"""
     return Product.new_product(
         {
             "name": "Samsung Galaxy S23 Ultra",
@@ -63,6 +72,7 @@ def new_product() -> Product:
 
 @pytest.fixture
 def cls_new_product():
+    """Fixture данные продукт в словаре. Смартфон Samsung"""
     return {
         "name": "Samsung Galaxy S23 Ultra",
         "description": "256GB, Серый цвет, 200MP камера",
@@ -87,7 +97,7 @@ def test_class_init_category(category_tv: Category, product1: Product) -> None:
         == "Современный телевизор, который позволяет наслаждаться просмотром, станет вашим другом и помощником"
     )
 
-    assert category_tv.products == "Samsung Galaxy S23 Ultra, 180000.0 руб. Остаток: 5 шт."
+    assert category_tv.products == "Телевизоры, количество продуктов: 5 шт."
     assert category_tv.category_count == ["Телевизоры"]
     assert category_tv.product_count == 1
 
@@ -125,7 +135,6 @@ def test_change_price_zero(new_product: Product, monkeypatch):
 def test_change_price_below_zero(new_product: Product, monkeypatch):
     """Тест на правильность работы метода price с отрицательным значением (ввод: '-800')
     из класса Product"""
-    monkeypatch.setattr("builtins.input", lambda _: "y")
     new_product.price = -800
     assert new_product.price == 180000
 
@@ -139,31 +148,59 @@ def test_new_product(cls_new_product: dict):
     assert new_product_test.quantity == 5
 
 
-
-def test_add_product(category_smartphones2, product_grass1, product_smartphone2):
+def test_add_product(category_smartphones1, product_grass1, product_smartphone2):
     """Тест на правильность добавления продукта в категорию"""
-    category_smartphones2.add_product(product_smartphone2)
-    assert category_smartphones2.products == 'Iphone 15, 210000.0 руб. Остаток: 8 шт.\nSamsung Galaxy S23 Ultra, 180000.0 руб. Остаток: 5 шт.'
+    category_smartphones1.add_product(product_smartphone2)
+    assert category_smartphones1.products == "Смартфоны, количество продуктов: 13 шт."
+    assert category_smartphones1.category_count == ["Телевизоры", "Смартфоны"]
+    assert category_smartphones1.product_count == 2
+    with pytest.raises(TypeError):
+        category_smartphones1.add_product("Not a product")
 
 
-def test_add_prod(product_smartphone1, product_smartphone2, product_grass1):
+def test_add_prod(product_smartphone1, product_smartphone2, product_grass1, capsys):
     """Тест на правильность работы функции сложения двух продуктов"""
+
     valid_sum = product_smartphone1 + product_smartphone2
-    assert valid_sum == 13
+    assert valid_sum == 2580000.0
 
     with pytest.raises(TypeError):
         product_smartphone1 + product_grass1
-        
-        
-def test_for_class_product__str__(product_1):
-    assert str(product_1) == "Samsung Galaxy S23 Ultra, 180000.0 руб. Остаток: 5 шт."
 
 
-def test_for_class_product__add__(product_1, product_2):
-    add_prod = product_1 + product_2
+def test_for_class_product__str__(product_smartphone1: Product):
+    assert str(product_smartphone1) == "Iphone 15, 210000.0 руб. Остаток: 8 шт."
+
+
+def test_for_class_product__add__(product_smartphone1, product_smartphone2):
+    add_prod = product_smartphone1 + product_smartphone2
     assert add_prod == 2580000.0
 
 
-def test_for_class_category__str__(category_1):
-    assert str(category_1) == "Samsung Galaxy S23 Ultra, 180000.0 руб. Остаток: 5 шт."
+def test_for_class_category__str__(category_smartphones1):
+    assert str(category_smartphones1) == "Iphone 15, 210000.0 руб. Остаток: 8 шт."
 
+
+def test_class_smartphone():
+    """тест на правильность работы класса Smartphone"""
+    smartphone2 = Smartphone("Iphone 15", "512GB, Gray space", 210000.0, 8, 98.2, "15", 512, "Gray space")
+    assert smartphone2.name == "Iphone 15"
+    assert smartphone2.description == "512GB, Gray space"
+    assert smartphone2.price == 210000.0
+    assert smartphone2.quantity == 8
+    assert smartphone2.efficiency == 98.2
+    assert smartphone2.model == "15"
+    assert smartphone2.memory == 512
+    assert smartphone2.color == "Gray space"
+
+
+def test_class_lawngrass():
+    """Тест на правильность работы класса LawnGrass"""
+    grass2 = LawnGrass("Газонная трава 2", "Выносливая трава", 450.0, 15, "США", "5 дней", "Темно-зеленый")
+    assert grass2.name == "Газонная трава 2"
+    assert grass2.description == "Выносливая трава"
+    assert grass2.price == 450.0
+    assert grass2.quantity == 15
+    assert grass2.country == "США"
+    assert grass2.germination_period == "5 дней"
+    assert grass2.color == "Темно-зеленый"
